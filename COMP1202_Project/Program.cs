2c40aa50a312b5afd7b3c5378f21cbf07b59d23f
Alfredd1 <gnavarroalfred@gmail.com>
2025-04-01 22:29:06 -0400

    Add the main file

---
+++ COMP1202_Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP1202_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
